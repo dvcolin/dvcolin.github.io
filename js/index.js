848ce3ece85c9a65980a1dd96ef0a370bb582e4c
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-06-19 08:31:26 -0700

    Added event listener to change color of active nav links

---
+++ js/index.js
@@ -0,0 +1,18 @@
+function sidebar() {
+    const aboutLink = document.querySelector('#about-link');
+    const sidebarLinks = document.querySelectorAll('.sidebar__nav .nav-link');
+    let activeLink = aboutLink;
+    
+    sidebarLinks.forEach(link => {
+        link.addEventListener('click', event => {
+            event.preventDefault();
+            if(link !== activeLink) {
+                activeLink.classList.remove('active-link');
+                activeLink = event.target;
+                activeLink.classList.add('active-link');
+            }
+        })
+    })
+}
+
+sidebar();
