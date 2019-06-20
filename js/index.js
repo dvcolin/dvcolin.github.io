e6325009d3b0231dbc62b97fcead5de15679b451
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-06-19 20:15:07 -0700

    Changed styles on about section, changed fonts, font sizes, padding

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 function sidebar() {
     const aboutLink = document.querySelector('#about-link');
     const sidebarLinks = document.querySelectorAll('.sidebar__nav .nav-link');
+    const ghLink = document.querySelector('#gh-link');
     let activeLink = aboutLink;
+    
     
     sidebarLinks.forEach(link => {
         link.addEventListener('click', event => {
-            event.preventDefault();
-            if(link !== activeLink) {
+            event.stopPropagation();
+            if(link !== activeLink && link !== ghLink) {
                 activeLink.classList.remove('active-link');
                 activeLink = event.target;
                 activeLink.classList.add('active-link');
@@ -19,6 +21,14 @@ function sidebar() {
 
 sidebar();
 
-const mobileNav = document.querySelector('.mobile__sidebar');
+const aboutButton = document.querySelector('.about__button');
+
+aboutButton.addEventListener('mouseover', () => {
+    aboutButton.classList.toggle('shadow-drop-2-center');
+});
+
+aboutButton.addEventListener('mouseout', () => {
+    aboutButton.classList.toggle('shadow-drop-2-center');
+})
 
 
